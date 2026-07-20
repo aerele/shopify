e42ba7656eb2f598c2127d6a48ad920bb2a07d84
Sachin1043 <sachinkanaga143@gmail.com>
2026-07-20 10:58:28 +0530

    fix(shopify_integration): create missing test fixtures and wire up hooks

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -14,7 +14,6 @@ githubbranch=${GITHUB_BASE_REF:-${GITHUB_REF##*/}}
 frappeuser=${FRAPPE_USER:-"frappe"}
 frappebranch=${FRAPPE_BRANCH:-$githubbranch}
 erpnextbranch=${ERPNEXT_BRANCH:-$githubbranch}
-paymentsbranch=${PAYMENTS_BRANCH:-${githubbranch%"-hotfix"}}
 
 git clone "https://github.com/${frappeuser}/frappe" --branch "${frappebranch}" --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
